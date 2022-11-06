b30a622037945b692b6c1b25356dce37ae200f09
Mauricio Lima <mauri.thor@gmail.com>
2022-11-06 14:28:20 -0300

    * Add `TvShowsContent` composable

---
+++ app/src/main/java/com/example/applaudochallenge/ui/navigation/components/TvShowsContent.kt
@@ -0,0 +1,41 @@
+package com.example.applaudochallenge.ui.navigation.components
+
+import android.widget.Toast
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.paging.compose.LazyPagingItems
+import com.example.applaudochallenge.R
+import com.example.applaudochallenge.network.LoadingUiState
+import com.example.applaudochallenge.network.getNetworkStatus
+import com.example.applaudochallenge.ui.models.TvShow
+import com.example.applaudochallenge.ui.widgets.ScreenState
+
+@Composable
+internal fun TvShowsContent(
+    loadingUiState: LoadingUiState?,
+    onEmptyButtonClick: () -> Unit,
+    onCardClick: (Int) -> Unit,
+    onRefresh: () -> Unit,
+    onCache: (TvShow) -> Unit
+) {
+    val context = LocalContext.current
+    val status = getNetworkStatus()
+
+    ScreenState<LazyPagingItems<TvShow>>(
+        loadingUiState = loadingUiState,
+        onClick = onEmptyButtonClick
+    ) {
+        TvShowList(it, onRefresh, onCache) { id ->
+            if (status) {
+                onCardClick(id)
+                return@TvShowList
+            } else {
+                Toast.makeText(
+                    context,
+                    context.getString(R.string.no_connection_found),
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+}
