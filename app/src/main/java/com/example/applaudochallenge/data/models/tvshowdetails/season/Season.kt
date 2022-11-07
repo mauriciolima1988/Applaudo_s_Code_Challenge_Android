d6849230b49b0723950daabfe2e33c698bd3055c
Mauricio Lima <mauri.thor@gmail.com>
2022-11-07 15:23:00 -0300

    * Add TMDB Season API data classes

---
+++ app/src/main/java/com/example/applaudochallenge/data/models/tvshowdetails/season/Season.kt
@@ -0,0 +1,19 @@
+package com.example.applaudochallenge.data.models.tvshowdetails.season
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.applaudochallenge.data.models.tvshowdetails.season.episode.Episode
+
+@Entity
+data class Season(
+    @PrimaryKey(autoGenerate = false)
+    val _id: String,
+    val air_date: String,
+    val episodes: List<Episode>,
+    val id: Int,
+    val name: String,
+    val overview: String,
+    val poster_path: String,
+    val season_number: Int
+)
+
---
+++ app/src/main/java/com/example/applaudochallenge/data/models/tvshowdetails/season/episode/Episode.kt
@@ -0,0 +1,18 @@
+package com.example.applaudochallenge.data.models.tvshowdetails.season.episode
+
+data class Episode(
+    val air_date: String,
+    val crew: List<Crew>,
+    val episode_number: Int,
+    val guest_stars: List<GuestStar>,
+    val id: Int,
+    val name: String,
+    val overview: String,
+    val production_code: String,
+    val season_number: Int,
+    val still_path: String,
+    val poster_path: String?,
+    val vote_average: Double,
+    val vote_count: Int,
+
+)
